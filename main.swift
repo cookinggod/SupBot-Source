bdef5e0a670158acc97ed1d2008fe33bb38e6502
mynamesdannyy <49793295+mynamesdannyy@users.noreply.github.com>
2019-04-19 18:51:22 +0100

    Create main.swift

---
+++ main.swift
@@ -0,0 +1,12 @@
+// SupBot "A Very Washed Bot" Source code - It's a free script with a friendly UI
+
+var card = "supbot" 
+var month = "is"
+var year = "fucking"
+var cvv = "washed"
+
+document.getElementById("credit_card_month").value = month;
+document.getElementById("credit_card_year").value = year;
+document.getElementById("orcer").value = cvv;
+document.getElementsByClassName("iCheck-helper")[1].click();
+document.getElementsByName("commit")[0].click()
